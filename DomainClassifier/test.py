9f4ae469d9d8f13f9bb10b694e0e958b52a4fcf1
Alexandre Dulaunoy <a@foo.be>
2012-01-24 11:49:07 +0100

    Test extended to show the use of localizedomain

--- DomainClassifier/test.py
+++ DomainClassifier/test.py
@@ -1,5 +1,12 @@
 import domainclassifier
 
-c = domainclassifier.Extract("this is a text with a domain called test@foo.lu another test abc.lu something a.b.c.d.e end of 1.2.3.4 foo.be")
+c = domainclassifier.Extract( rawtext = "this is a text with a domain called test@foo.lu another test abc.lu something a.b.c.d.e end of 1.2.3.4 foo.be www.belnet.be http://www.cert.be/ www.public.lu www.allo.lu quuxtest www.eurodns.com something-broken-www.google.com www.google.lu trailing test")
+
 print c.domain()
-print c.validdomain(extended=None)
+print c.validdomain(extended=False)
+print "US:"
+print c.localizedomain(cc='US')
+print "LU:"
+print c.localizedomain(cc='LU')
+print "BE:"
+print c.localizedomain(cc='BE')
