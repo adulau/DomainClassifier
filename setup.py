94482dcfbfcf0923c59efa5aa2d5b0d024049ecd
Alexandre Dulaunoy <a@foo.be>
2014-08-18 21:56:11 +0200

    Setup script for DomainClassifier

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name = "DomainClassifier",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = ['dnspython', 'IPy'],
+    author = "Alexandre Dulaunoy",
+    author_email = "a@foo.be",
+    description = " DomainClassifier is a simple Python library to extract and classify Internet domains/hostnames/IP addresses from raw text files following their existence, localization or attributes.",
+    license = "AGPL",
+    keywords = "internet mining domain resolver geolocalisation",
+    url = "http://github.com/adulau/DomainClassifier"
+)
