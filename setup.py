237dd6bd91b92412a918e1acd7756c6734f0b090
Alexandre Dulaunoy <a@foo.be>
2014-09-04 07:57:17 +0200

    Python package version 0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="DomainClassifier",
-    version="0.3",
+    version="0.4",
     packages=find_packages(),
     install_requires=['dnspython', 'IPy'],
     author="Alexandre Dulaunoy",
