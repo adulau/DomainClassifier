eb38342b231710c198a5a8afb9727a4a4d7e076e
Alexandre Dulaunoy <a@foo.be>
2017-07-14 08:27:50 +0200

    0.7 release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="DomainClassifier",
-    version="0.4",
+    version="0.6",
     packages=find_packages(),
     install_requires=['dnspython', 'IPy'],
     author="Alexandre Dulaunoy",
