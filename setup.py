b6818de02d437fa435da0071ae1a5808c6f18bc3
Alexandre Dulaunoy <a@foo.be>
2018-04-20 10:44:55 +0200

    0.7 released and pushed to PyPI

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="DomainClassifier",
-    version="0.6",
+    version="0.7",
     packages=find_packages(),
     install_requires=['dnspython', 'IPy'],
     author="Alexandre Dulaunoy",
