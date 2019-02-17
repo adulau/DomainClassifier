04223122774985d2e70c9e4eca1796c3134a9ab0
Alexandre Dulaunoy <a@foo.be>
2019-02-17 10:25:42 +0100

    chg: [release] v0.8

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 setup(
     name="DomainClassifier",
-    version="0.7",
+    version="0.8",
     packages=find_packages(),
-    install_requires=['dnspython', 'IPy'],
+    install_requires=['dnspython', 'IPy', 'pybgpranking'],
+    dependency_links=[
+      'git+https://github.com/D4-project/BGP-Ranking.git/@7e698f87366e6f99b4d0d11852737db28e3ddc62#egg=pybgpranking&subdirectory=client',
+    ],
     author="Alexandre Dulaunoy",
     author_email="a@foo.be",
-    description="DomainClassifier is a Python library to extract and classify Internet domains/hostnames/IP addresses from raw text files following their existence, localization or attributes.",
+    description="DomainClassifier is a Python library to extract and classify Internet domains/hostnames/IP addresses from raw unstructured text files following their existence, localization or attributes.",
     license="AGPL",
     keywords="internet mining domain resolver geolocalisation",
     url="http://github.com/adulau/DomainClassifier"
