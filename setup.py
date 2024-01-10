caed4bfddc06b1d87a8f698c5f015f31a5bdec9b
Alexandre Dulaunoy <a@foo.be>
2024-01-10 16:32:45 +0100

    chg: [dep] remove PyBGPranking for the time being

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     version="1.1",
     packages=find_packages(),
     install_requires=['dnspython', 'IPy', 'pybgpranking'],
-    dependency_links=[
-      'git+https://github.com/D4-project/BGP-Ranking.git/@7e698f87366e6f99b4d0d11852737db28e3ddc62#egg=pybgpranking&subdirectory=client',
-    ],
     author="Alexandre Dulaunoy",
     author_email="a@foo.be",
     description="DomainClassifier is a Python library to extract and classify Internet domains/hostnames/IP addresses from raw unstructured text files following their existence, localization or attributes.",
